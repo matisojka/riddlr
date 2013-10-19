1980697e9f7396bd0660d771b3a2282c49bd3dfe
Mateusz Sójka <yagooar@gmail.com>
2013-10-19 07:58:01 +0200

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Riddlr::Application.config.secret_key_base = '447b044b256354e6a477f5cfd2e399223e97bd266f58a24ad991b69cf611cc6a37073bd943e6282d1ed52c042f46da1c142ee92caf502a4ad3127d21c48b7544'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Riddlr::Application.config.session_store :cookie_store, key: '_riddlr_session'
